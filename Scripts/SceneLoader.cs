91ee692bbcba7de33e05810ddb2c7b2cd8ee4b6e
iggdrasil14 <iggdrasil14@gmail.com>
2024-05-26 21:56:29 +0700

    26.05.2024 - 2

---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GameStart()     //Метод запуска приложения.
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SceneOptions()  //Метод запуска экрана опции/настройки.
+    {
+        //SceneManager.LoadScene();
+        Debug.Log("Запуск сцены Options");
+    }
+
+    public void SceneCredits()  //Метод запуска экрана кредиты.
+    {
+        //SceneManager.LoadScene();
+        Debug.Log("Запуск сцены Credits");
+    }
+
+    public void ExitGame()      //Метод выхода из приложения.
+    {
+        Application.Quit();
+    }
+
+}
