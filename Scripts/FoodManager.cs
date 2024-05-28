2be47d64381944e1d67cd200b872275ed0c553fd
iggdrasil14 <iggdrasil14@gmail.com>
2024-05-28 23:02:14 +0700

    28.05.2024 - 2

---
+++ Scripts/FoodManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FoodManager : MonoBehaviour
+{
+    public int food;
+    public TextMeshProUGUI foodText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodText.text = food.ToString();   
+    }
+
+    public void SpendFood()
+    {
+        if (food > 0)
+        {
+            food -= 1;
+            foodText.text = food.ToString();
+        }
+    }
+
+    public void GetFood()
+    {
+        if (MoneyManager.IsCanBuy == true)
+        {
+            food += 10;
+            foodText.text = food.ToString();
+        }
+    }
+}
---
+++ Scripts/MoneyManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyManager : MonoBehaviour
+{
+    private static bool isCanBuy;
+    public static bool IsCanBuy
+    {
+        get 
+        {
+            bool r = isCanBuy;
+            isCanBuy = false;
+            return r;
+        }
+        private set => isCanBuy = value;
+    }
+    public int money;
+    public TextMeshProUGUI textMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMoney.text = money.ToString();
+    }
+
+    public void SpendMoney(int spendMoney)              // Метод траты денег.
+    {
+        if (money >= spendMoney)
+        {
+            money -= spendMoney;
+            textMoney.text = money.ToString();
+            IsCanBuy = true;
+        }
+    }
+
+    public void SetMoney(int setMoney)                 // Метод получения денег.
+    {
+        if (setMoney > 0)
+        {
+            money += setMoney;
+            textMoney.text = money.ToString();
+        }
+    }
+}
